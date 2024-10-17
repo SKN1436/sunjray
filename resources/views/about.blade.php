bc2752402ef7030c8270dbca30323fe07242b9e3
subrat kumar nayak <subratnayak1436@gmail.com>
2024-10-17 12:50:02 +0530

    add my first project

---
+++ resources/views/about.blade.php
@@ -0,0 +1,4 @@
+@extends('layout.app')
+@section('content')
+<p>This is the about page</p>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.app')
+@section('content')
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis, accusantium.</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, laborum.</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, possimus?</p>
+@endsection
